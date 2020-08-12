b38e5b459cab4269173e85cb6feec5b61c8585df
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-12 15:54:52 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+ $(document).ready(function(){
+   $('a[href*=#].header-bottom-link').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 1400);
+      e.preventDefault();
+   });
+   return false;
+});
+
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#979797" });
+ });
+ 
+ 	/*Fancybox*/
+$(document).ready(function(){
+
+            var forms = $('.form-modal'),
+            cb_input = forms.find('input[type=text], input[type=email], input[type=tel] , textarea'),
+            cr_close = $.fancybox.close();
+
+    $("#fancybox-overlay").fancybox({
+        padding: [0, 0, 0, 0],
+        margin: [120, 0, 0, 0],
+        scrolling: 'visible',
+        minWidth: 320,
+        minHeight: 294,
+        autoSize: false,
+        autoHeight: true,
+        autoWidth: true,
+        maxWidth: 9999,
+        maxHeight: 9999,
+        fixed: false,
+        autoCenter: false,
+        closeBtn: false,
+        afterClose: function () {
+            cb_input.val('');
+        }
+    });
+
+    $('.btr').click(function () {
+        $.fancybox.close();
+    });
+	
+   });
+/*Fancybox end*/  
+
+	
+		
+		//   $("a[href$='.jpg'], a[href$='.png']")
+//           .addClass("fancybox");
+          
+           $("a[href$='.jpg'], a[href$='.png']").each(function() {
+      $(this).addClass("fancybox");
+       $(this).attr('rel', 'gallery');
+     
+});
+
+ $(".fancybox").fancybox({
+        padding: [0, 0, 0, 0],
+        openEffect: 'elastic',
+        openSpeed: 300,
+//        closeEffect: 'elastic',
+//        scrolling: 'visible',
+         background:'white',
+        maxWidth: 800,
+		arrows: false,
+        fixed: false,
+        autoCenter: false,
+     
+    });
+	
+	$(function() {
+     var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            paginationClickable: true,
+            spaceBetween: 10,
+            centeredSlides: true,
+            autoplay: 3000,
+            autoplayDisableOnInteraction: false
+           
+        });
+
+    }); 
+
+
